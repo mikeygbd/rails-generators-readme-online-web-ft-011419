009c6320ec0f43f861ebce34a52ae4fa311406df
mikeygbd <mikey.soares@gmail.com>
2019-03-19 14:37:10 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,7 +19,7 @@ class PostsController < ApplicationController
 
   def update
     @post = Post.find(params[:id])
-    @post.update(params.require(:post))
+    @post.update(title: params[:post][:title], description: params[:post][:description])
     redirect_to post_path(@post)
   end
 
